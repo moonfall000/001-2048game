ece4b4038501b1982a2d60e2f6ddc6298ab102b4
moonfall000 <senryckeyminecraft@gmail.com>
2026-08-14 02:11:07 +0800

    fix

--- src/app/page.js
+++ src/app/page.js
@@ -828,9 +828,9 @@ export default function Game() {
       //貓咪算法
       // 🎯 規律衰減核心：計算這隻貓咪的「咬合係數」
       // 除以 catNum 的增長，讓加權幅度以規律的斜率瘋狂縮小！ 基本上50名後吃不到加權
-      let Numofcat = (600 - catNum * 12);
+      let Numofcat = (575 - catNum * 12);
       if (Numofcat <= 0) { Numofcat = 1};
-      const weightFactor = (Numofcat) * 0.0016 / Math.pow(catNum, 0.95); // 0.65次方控制衰減平滑度，名次越後越咬不動
+      const weightFactor = (Numofcat) * 0.0012 / Math.pow(catNum, 0.995); // 0.65次方控制衰減平滑度，名次越後越咬不動
       
       // 🎮 基礎保底戰力也隨著排名由大到小規律遞減
       const basePower = Math.max(10, 5000 - catNum * 48) * (1 + chestLevel * 0.05);
